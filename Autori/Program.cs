e0e5af92acddf08b3aaaf8026008f88e91e86aa3
Erik <erik.ra@gmail.com>
2017-05-10 16:24:35 +0200

    Add project files.

---
+++ Autori/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split('-');
+            string output = string.Empty;
+
+            foreach (var input in inputs)
+            {
+                output += input[0].ToString();
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
